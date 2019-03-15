e00fac5b9ff9207e281755c40db82ad9cf2c1ad0
Murilo <murilo@stavares.com>
2019-03-15 14:42:27 -0300

    new 1

--- app/src/main/java/com/example/miwok/NumberActivity.java
+++ app/src/main/java/com/example/miwok/NumberActivity.java
@@ -1,7 +1,10 @@
 package com.example.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+        ArrayList<String> numberList = new ArrayList<String>();
+        numberList.add("one");
+        numberList.add("two");
+        numberList.add("three");
+        numberList.add("four");
+        numberList.add("five");
+        numberList.add("six");
+        numberList.add("seven");
+        numberList.add("eight");
+        numberList.add("nine");
+        numberList.add("ten");
     }
 }
